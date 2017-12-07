49a6dc094ca0cbdccdec7d62d2d96d4168a3a229
Ashish Mishra <ashish.25jl@gmail.com>
2017-12-07 23:53:22 +0530

    The feature module with named outlets is working. Lazy loadin is the next milestone !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,8 @@ import {root_routes_in_my_application} from './app.routes';
 import { QuestionBankComponent } from './question-bank/question-bank.component';
 import { CreateTestComponent } from './create-test/create-test.component';
 import { TakeTestComponent } from './take-test/take-test.component';
-import { ManageTestComponent } from './manage-test/manage-test.component';
-import { CompletedTestsComponent } from './manage-test/completed-tests/completed-tests.component';
-import { RemainingTestsComponent } from './manage-test/remaining-tests/remaining-tests.component';
-import { TestStatsComponent } from './manage-test/test-stats/test-stats.component';
-import { CompComponent } from './comp/comp.component';
+import { AdminModule } from './admin/admin.module';
+
 
 
 @NgModule({
@@ -28,15 +25,11 @@ import { CompComponent } from './comp/comp.component';
     QuestionBankComponent,
     CreateTestComponent,
     TakeTestComponent,
-    ManageTestComponent,
-    CompletedTestsComponent,
-    RemainingTestsComponent,
-    TestStatsComponent,
-    CompComponent
   ],
   imports: [
-    BrowserModule,RouterModule.forRoot(root_routes_in_my_application)
+    BrowserModule,AdminModule,RouterModule.forRoot(root_routes_in_my_application)
   ],
+  // 
   providers: [],
   bootstrap: [AppComponent]
 })
